650d7150cd4a1d631e608bf9ad089b02e0695fba
Einer Santana R <einersantanar@hotmail.com>
2016-01-06 19:25:27 -0400

    Cambio de tipo de dato en el conection string
    Se cambio de string a SecureString para proporcionar seguridad a las
    conexiones establecidas por codigo.

--- EmpleoDotNet/Models/Empleos2Net.cs
+++ EmpleoDotNet/Models/Empleos2Net.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Web;
 
 namespace EmpleoDotNet.Models
@@ -16,7 +17,7 @@ namespace EmpleoDotNet.Models
         /// <summary>
         /// Get all set the connection string of the database 
         /// </summary>
-        public static string ConnectionString { get; set; }
+        public static SecureString ConnectionString { get; set; }
 
         /// <summary>
         /// Set the service implementation to use depending 
