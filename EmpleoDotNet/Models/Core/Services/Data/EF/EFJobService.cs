8aeb005532263a7b59f91bba16d6166a6ef257f3
Einer Santana R <einersantanar@hotmail.com>
2016-01-06 19:27:59 -0400

    Implementacion del IJobService de EF
    Implementacion del servicio IJobService con Entity Framework.

---
+++ EmpleoDotNet/Models/Core/Services/Data/EF/EFJobService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace EmpleoDotNet.Models.Core.Services.Data.EF
+{
+    internal class EFJobService : BaseEFService<JobOpportunity>, IJobsService
+    {
+        public EFJobService()
+        {
+           
+        }
+
+        public IEnumerable<JobOpportunity> GetAllJobs()
+        {
+            var jobs = this.GetAll().Include(x => x.Location);
+            return jobs;
+        }
+
+
+
+        public IEnumerable<JobOpportunity> GetJobsByCategory(JobCategory category)
+        {
+            return GetAllJobs().Where(x => x.Category == category);
+        }
+
+        public void Dispose()
+        {
+            this.Context.Dispose();
+        }
+
+        public JobOpportunity GetJobById(int id)
+        {
+            return this.FindById(id);
+        }
+
+        protected override JobOpportunity FindById(int id)
+        {
+            var job = this.DbSet.Include(x => x.Location).FirstOrDefault(x => x.Id == id); ;
+            return job;
+        }
+    }
+}
