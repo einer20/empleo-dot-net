27f6bb33603a9aff81567b9aad40fba3e2011730
Einer Santana R <einersantanar@hotmail.com>
2016-01-07 00:15:40 -0400

    Js de la busqueda rapida agregado
    Se agrego el js de la busqueda rapida para poder buscar trabajos en
    todas partes de la aplicacion

--- EmpleoDotNet/Scripts/app.js
+++ EmpleoDotNet/Scripts/app.js
@@ -4,3 +4,63 @@
         window.location = detailUrl;
     }
 };
+
+$(document).ready(function () {
+    var quickSearchTemplate = [
+        '<li class="quick-search-item">',
+            '<div>',
+                '<div class="info">',
+                    '<div><span class="expand glyphicon glyphicon-plus"></span><span class="name"></span></div>',
+                    '<span class="description hide"></span>',
+                    '<a class="view-more" href="">Mas...</a> | <a href="" class="c-web-site">Web site</a>',
+                '</div>',
+            '</div>',
+        '</li>'
+    ].join(' ');
+    var quickSearchResultContainer = $("#quick-search-result");
+
+    $("#quick-search")
+        .blur(function () {
+            quickSearchResultContainer.hide();
+        })
+        .keyup(function () {
+            var $ul = quickSearchResultContainer.show().find("ul");
+
+            if (this.value == "") {
+                quickSearchResultContainer.hide();
+            }
+            else {
+                quickSearchResultContainer.show();
+            }
+
+            $.get(href("~/JobOpportunity/quickjobsearch"), { hint: this.value }, function (response) {
+
+                if (response) {
+                    $ul.find("li").remove();
+                    for (var i = 0, total = response.length; i < total; i++) {
+                        var current = response[i];
+
+                        for (var y = 0; y < current.length; y++) {
+                            var job = current[y];
+                            var $element = $($.parseHTML(quickSearchTemplate));
+                            $element.find(".category").text(job.categoryString);
+                            $element.find(".description").text(job.description);
+                            $element.find(".name").text(job.name);
+                            $element.find(".c-web-site").attr("href", job.companyUrl);
+                            $element.find(".view-more").attr("href", href("~/JobOpportunity/detail?id=" + job.id));
+                            $element.find(".expand").hover(function () {
+                                $element
+                                    .find(".description")
+                                    .toggleClass("hide");
+                            });
+
+                            $ul.append($element);
+                        }
+                    }
+                }
+            });
+        });
+})
+
+
+
